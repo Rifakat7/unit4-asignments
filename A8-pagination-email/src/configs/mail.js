87fbf6199973124be99becf0eb207ad25b27a279
Rifakat7 <97470134+Rifakat7@users.noreply.github.com>
2022-04-03 22:17:46 +0530

    Add files via upload

---
+++ A8-pagination-email/src/configs/mail.js
@@ -0,0 +1,11 @@
+const nodemailer = require("nodemailer");
+
+module.exports = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "bfe187130f6caf",
+    pass: "f104c13a87603b",
+  },
+});
